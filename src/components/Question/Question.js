59e834a7f0a044eb769cd58f5ff8ef9b5e7f98e8
fazlarabby1 <fazlarabby41@gmail.com>
2022-10-11 19:53:50 +0600

    added eye icon and showing the correct answer as toast

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { toast } from 'react-toastify';
 import Option from '../Option/Option';
@@ -13,13 +15,19 @@ const Question = ({ topicQuestion, index }) => {
         toast.warning('Your answer is incorrect', {autoClose: 500})
         
     }
+    const showCorrectAnswer = () =>{
+        toast.info(`Correct answer: <b>${correctAnswer}</b>`, {autoClose: 2000})
+    }
     return (
         <div className='container'>
             <div className="card my-4 shadow">
                 <div className="card-body">
-                    <h5 className="card-title">Quiz {index + 1}:
+                    <div className='d-flex align-items-center'>
+                    <h5 className="card-title mx-auto">Quiz {index + 1}:
                         {question}
                     </h5>
+                    <FontAwesomeIcon onClick={showCorrectAnswer} className='text-end' title='Show The Correct Answer' icon={faEye} ></FontAwesomeIcon>
+                    </div>
                     <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 mt-2">
                     {
                         options.map(option => <Option key={options.indexOf(option)} number={options.indexOf(option)} handleAnswerValidation={handleAnswerValidation} option={option}></Option>)
